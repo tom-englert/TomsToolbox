2ba5b7d43a909ff13386fd2090ef49ff4c185645
tom-englert <mail@tom-englert.de>
2016-02-16 14:16:37 +0100

    Define xmlns prefix

--- TomsToolbox.Wpf/Properties/AssemblyInfo.cs
+++ TomsToolbox.Wpf/Properties/AssemblyInfo.cs
@@ -15,6 +15,7 @@ using System.Windows.Markup;
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 
+[assembly: XmlnsPrefix("urn:TomsToolbox", "toms")]
 [assembly: XmlnsDefinition("urn:TomsToolbox", "TomsToolbox.Wpf")]
 [assembly: XmlnsDefinition("urn:TomsToolbox", "TomsToolbox.Wpf.Interactivity")]
 [assembly: XmlnsDefinition("urn:TomsToolbox", "TomsToolbox.Wpf.Composition")]
