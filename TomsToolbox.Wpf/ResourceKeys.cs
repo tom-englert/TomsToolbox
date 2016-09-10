566519fa4968ad8bb08855b459dc7f7f2267c0c2
tom-englert <mail@tom-englert.de>
2016-09-10 07:02:17 +0200

    Change resource keys to workaround https://connect.microsoft.com/VisualStudio/feedback/details/2993889/

--- TomsToolbox.Wpf/ResourceKeys.cs
+++ TomsToolbox.Wpf/ResourceKeys.cs
@@ -8,38 +8,66 @@
     public static class ResourceKeys
     {
         /// <summary>
+        /// The key name for the <see cref="AutoToolTipTextBoxStyle"/>
+        /// </summary>
+        public static readonly string AutoToolTipTextBoxStyleKeyName = "AutoToolTipTextBoxStyle";
+        /// <summary>
         /// A style for text boxes that automatically sets the tool tip if the text is trimmed.
         /// </summary>
-        public static readonly ResourceKey AutoToolTipTextBoxStyle = new ComponentResourceKey(typeof(ResourceKeys), "AutoToolTipTextBoxStyle");
+        public static readonly ResourceKey AutoToolTipTextBoxStyle = new ComponentResourceKey(typeof(ResourceKeys), AutoToolTipTextBoxStyleKeyName);
 
+        /// <summary>
+        /// The key name for the <see cref="SharedWidthContainerStyle"/>
+        /// </summary>
+        public static readonly string SharedWidthContainerStyleKeyName = "SharedWidthContainerStyle";
         /// <summary>
         /// Resource key for a style applying a shared with to associated container controls.
         /// </summary>
-        public static readonly ResourceKey SharedWidthContainerStyle = new ComponentResourceKey(typeof(ResourceKeys), "SharedWidthContainerStyle");
+        public static readonly ResourceKey SharedWidthContainerStyle = new ComponentResourceKey(typeof(ResourceKeys), SharedWidthContainerStyleKeyName);
 
+        /// <summary>
+        /// The key name for the <see cref="ListBoxItemCheckBoxStyle"/>
+        /// </summary>
+        public static readonly string ListBoxItemCheckBoxStyleKeyName = "ListBoxItemCheckBoxStyle";
         /// <summary>
         /// List box/list view with check boxes: Style to be applied to the check box inside item or cell template. See e.g. http://msdn.microsoft.com/en-us/library/ms754143.aspx.
         /// </summary>
-        public static readonly ResourceKey ListBoxItemCheckBoxStyle = new ComponentResourceKey(typeof(ResourceKeys), "ListBoxItemCheckBoxStyle");
+        public static readonly ResourceKey ListBoxItemCheckBoxStyle = new ComponentResourceKey(typeof(ResourceKeys), ListBoxItemCheckBoxStyleKeyName);
 
+        /// <summary>
+        /// The key name for the <see cref="DataGridRowCheckBoxStyle"/>
+        /// </summary>
+        public static readonly string DataGridRowCheckBoxStyleKeyName = "DataGridRowCheckBoxStyle";
         /// <summary>
         /// Data grid with check boxes for row selection: Style to be applied to the check box inside the row header template.
         /// </summary>
-        public static readonly ResourceKey DataGridRowCheckBoxStyle = new ComponentResourceKey(typeof(ResourceKeys), "DataGridRowCheckBoxStyle");
+        public static readonly ResourceKey DataGridRowCheckBoxStyle = new ComponentResourceKey(typeof(ResourceKeys), DataGridRowCheckBoxStyleKeyName);
 
+        /// <summary>
+        /// The key name for the <see cref="DataGridCellStyle"/>
+        /// </summary>
+        public static readonly string DataGridCellStyleKeyName = "DataGridCellStyle";
         /// <summary>
         /// A copy of the original data grid cell style, extended with VerticalAlignment binding to control the vertical alignment of the content via the DataGrid.VerticalContentAlignment property. Also adds support for padding.
         /// </summary>
-        public static readonly ResourceKey DataGridCellStyle = new ComponentResourceKey(typeof(ResourceKeys), "DataGridCellStyle");
+        public static readonly ResourceKey DataGridCellStyle = new ComponentResourceKey(typeof(ResourceKeys), DataGridCellStyleKeyName);
 
+        /// <summary>
+        /// The key name for the <see cref="ControlWithValidationErrorToolTipStyle"/>
+        /// </summary>
+        public static readonly string ControlWithValidationErrorToolTipStyleKeyName = "ControlWithValidationErrorToolTipStyle";
         /// <summary>
         /// A control that shows validation errors in the tool tip.
         /// </summary>
-        public static readonly ResourceKey ControlWithValidationErrorToolTipStyle = new ComponentResourceKey(typeof(ResourceKeys), "ControlWithValidationErrorToolTipStyle");
+        public static readonly ResourceKey ControlWithValidationErrorToolTipStyle = new ComponentResourceKey(typeof(ResourceKeys), ControlWithValidationErrorToolTipStyleKeyName);
 
+        /// <summary>
+        /// The key name for the <see cref="CompositeMenuStyle"/>
+        /// </summary>
+        public static readonly string CompositeMenuStyleKeyName = "CompositeMenuStyle";
         /// <summary>
         /// A style to build composite menus.
         /// </summary>
-        public static readonly ResourceKey CompositeMenuStyle = new ComponentResourceKey(typeof(ResourceKeys), "CompositeMenuStyle");
+        public static readonly ResourceKey CompositeMenuStyle = new ComponentResourceKey(typeof(ResourceKeys), CompositeMenuStyleKeyName);
     }
 }
