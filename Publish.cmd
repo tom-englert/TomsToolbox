e1934244be47d49e574c8723d7780b3ab3de5cc1
tom-englert <mail@tom-englert.de>
2016-11-19 17:15:56 +0100

    Adapt Publish.cmd for build server

--- Publish.cmd
+++ Publish.cmd
@@ -1,16 +1,12 @@
-SET VERSION=1.0.52.0
-
-PUSHD "%~dp0Deploy"
+IF /I NOT "%1" == "true" GOTO :EOF
 
 CALL :PUSH Core
 CALL :PUSH Desktop
 CALL :PUSH ObservableCollections
 CALL :PUSH Wpf
 CALL :PUSH Wpf.Styles
-
-PAUSE
 GOTO :EOF
 
 :PUSH
-"%~dp0.nuget\nuget" push TomsToolbox.%1.%VERSION%.nupkg
+"%~dp0.nuget\nuget" push TomsToolbox.%1.1.0.??.0.nupkg
 GOTO :EOF
