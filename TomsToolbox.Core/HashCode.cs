0f0d31f534e194dece9fe530147e0d5d89f5f52c
tom-englert <mail@tom-englert.de>
2017-02-07 13:24:42 +0100

    Added HashCode class

---
+++ TomsToolbox.Core/HashCode.cs
@@ -0,0 +1,22 @@
+﻿namespace TomsToolbox.Core
+{
+    /// <summary>
+    /// Helper to generate hash codes
+    /// </summary>
+    public static class HashCode
+    {
+        /// <summary>
+        /// Aggregates the specified values to a single hash code.
+        /// </summary>
+        /// <param name="h1">The first hash code.</param>
+        /// <param name="h2">The second hash code.</param>
+        /// <returns>A new hash code calculated from the specified ones.</returns>
+        public static int Aggregate(int h1, int h2)
+        {
+            unchecked
+            {
+                return ((h1 << 5) + h1) ^ h2;
+            }
+        }
+    }
+}
