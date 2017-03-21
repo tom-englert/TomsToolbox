841a07cb73618056366f1f50bb634688b9e48406
tom-englert <mail@tom-englert.de>
2017-03-21 15:12:58 +0100

    CustomNonClientAreaBehavior: Schedule a redraw, if DWM composition is disabled, since DefWindowProc(...WM_NCACTIVATE..) *does* some extra drawing on the window frame.

--- TomsToolbox.Wpf/Interactivity/CustomNonClientAreaBehavior.cs
+++ TomsToolbox.Wpf/Interactivity/CustomNonClientAreaBehavior.cs
@@ -260,10 +260,7 @@
             var width = nonClientArea.ActualWidth;
             var height = nonClientArea.ActualHeight;
 
-            window.Dispatcher?.BeginInvoke(DispatcherPriority.Loaded, () =>
-            {
-                ApplySizeToContent(window, sizeToContent, width, height);
-            });
+            window.Dispatcher?.BeginInvoke(DispatcherPriority.Loaded, () => ApplySizeToContent(window, sizeToContent, width, height));
         }
 
         private static void ApplySizeToContent([NotNull] Window window, SizeToContent sizeToContent, double width, double height)
@@ -360,8 +357,10 @@
                     break;
 
                 case WM_NCACTIVATE:
-                    // Must call DefWindowProc with lParam set to -1, else windows might do some custom drawing in the NC area.
                     handled = true;
+                    // Schedule a redraw, if DWM composition is disabled, DefWindowProc(...WM_NCACTIVATE..) *does* some extra drawing.
+                    NativeMethods.RedrawWindow(windowHandle, IntPtr.Zero, IntPtr.Zero, RedrawWindowFlags.Invalidate | RedrawWindowFlags.NoErase);
+                    // Must call DefWindowProc with lParam set to -1, else windows might do some custom drawing in the NC area.
                     return NativeMethods.DefWindowProc(windowHandle, WM_NCACTIVATE, wParam, new IntPtr(-1));
 
                 case WM_NCHITTEST:
@@ -646,6 +645,56 @@
             public readonly POINT ptMaxTrackSize;
         };
 
+        [Flags]
+        private enum RedrawWindowFlags : uint
+        {
+            /// <summary>
+            /// Invalidates the rectangle or region that you specify in lprcUpdate or hrgnUpdate.
+            /// You can set only one of these parameters to a non-NULL value. If both are NULL, RDW_INVALIDATE invalidates the entire window.
+            /// </summary>
+            Invalidate = 0x1,
+
+            /// <summary>Causes the OS to post a WM_PAINT message to the window regardless of whether a portion of the window is invalid.</summary>
+            InternalPaint = 0x2,
+
+            /// <summary>
+            /// Causes the window to receive a WM_ERASEBKGND message when the window is repainted.
+            /// Specify this value in combination with the RDW_INVALIDATE value; otherwise, RDW_ERASE has no effect.
+            /// </summary>
+            Erase = 0x4,
+
+            /// <summary>
+            /// Validates the rectangle or region that you specify in lprcUpdate or hrgnUpdate.
+            /// You can set only one of these parameters to a non-NULL value. If both are NULL, RDW_VALIDATE validates the entire window.
+            /// This value does not affect internal WM_PAINT messages.
+            /// </summary>
+            Validate = 0x8,
+
+            NoInternalPaint = 0x10,
+
+            /// <summary>Suppresses any pending WM_ERASEBKGND messages.</summary>
+            NoErase = 0x20,
+
+            /// <summary>Excludes child windows, if any, from the repainting operation.</summary>
+            NoChildren = 0x40,
+
+            /// <summary>Includes child windows, if any, in the repainting operation.</summary>
+            AllChildren = 0x80,
+
+            /// <summary>Causes the affected windows, which you specify by setting the RDW_ALLCHILDREN and RDW_NOCHILDREN values, to receive WM_ERASEBKGND and WM_PAINT messages before the RedrawWindow returns, if necessary.</summary>
+            UpdateNow = 0x100,
+
+            /// <summary>
+            /// Causes the affected windows, which you specify by setting the RDW_ALLCHILDREN and RDW_NOCHILDREN values, to receive WM_ERASEBKGND messages before RedrawWindow returns, if necessary.
+            /// The affected windows receive WM_PAINT messages at the ordinary time.
+            /// </summary>
+            EraseNow = 0x200,
+
+            Frame = 0x400,
+
+            NoFrame = 0x800
+        }
+
         private static class NativeMethods
         {
             [DllImport("user32.dll", CharSet = CharSet.Unicode)]
@@ -674,6 +723,9 @@
 
             [DllImport("user32.dll")]
             public static extern int EnableMenuItem(IntPtr hMenu, uint uIDEnableItem, uint uEnable);
+
+            [DllImport("user32.dll")]
+            public static extern bool RedrawWindow(IntPtr hWnd, IntPtr lprcUpdate, IntPtr hrgnUpdate, RedrawWindowFlags flags);
         }
     }
 }
