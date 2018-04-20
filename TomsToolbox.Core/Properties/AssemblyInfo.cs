ac936bb2133ad41b030df27d45e82b69ad9be3da
tom-englert <mail@tom-englert.de>
2018-04-20 19:11:48 +0200

    Migrate core

--- TomsToolbox.Core/Properties/AssemblyInfo.cs
+++ TomsToolbox.Core/Properties/AssemblyInfo.cs
@@ -3,9 +3,6 @@ using System.Reflection;
 using System.Resources;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("TomsToolbox.Core")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 [assembly: CLSCompliant(true)]
