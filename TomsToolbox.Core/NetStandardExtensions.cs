546bc3a06bca96e4edf552b9524813c23217507e
tom-englert <mail@tom-englert.de>
2018-07-21 17:42:18 +0200

    NetStandardExtensions should not be public visible

--- TomsToolbox.Core/NetStandardExtensions.cs
+++ TomsToolbox.Core/NetStandardExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace TomsToolbox.Core
+﻿#if !NETSTANDARD1_0
+
+namespace TomsToolbox.Core
 {
     using System;
     using System.Diagnostics.CodeAnalysis;
@@ -18,20 +20,12 @@
         /// <returns>The <see cref="MethodInfo"/></returns>
         [NotNull]
         [SuppressMessage("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode", Justification = "CA seems to be wrong about this!")]
-#if !NETSTANDARD1_0
         public static MethodInfo GetMethodInfo([NotNull] this Delegate @delegate)
         {
 
             // ReSharper disable once AssignNullToNotNullAttribute
             return @delegate.Method;
         }
-#else
-        // stub to generate the same external annotations, should not be used at all
-        public static MethodInfo GetMethodInfo([NotNull] Delegate @delegate)
-        {
-            throw new NotImplementedException();
-        }
-#endif
 
         /// <summary>
         /// Gets the type information for a type.
@@ -39,18 +33,12 @@
         /// <param name="type">The type.</param>
         /// <returns>The specified type.</returns>
         [NotNull]
-#if !NETSTANDARD1_0
         public static Type GetTypeInfo([NotNull] this Type type)
         {
 
             return type;
         }
-#else
-        // stub to generate the same external annotations, should not be used at all
-        public static Type GetTypeInfo([NotNull] Type type)
-        {
-            throw new NotImplementedException();
-        }
-#endif
     }
 }
+
+#endif
