3451bd807a2ece1921b12c6ab80c8eb98f65737a
tom-englert <mail@tom-englert.de>
2019-06-16 18:12:10 +0200

    Migrate sample app to new project format (giving up click once)

--- SampleApp/Properties/AssemblyInfo.cs
+++ SampleApp/Properties/AssemblyInfo.cs
@@ -1,21 +1,8 @@
-﻿using System.Reflection;
-using System.Resources;
-using System.Runtime.InteropServices;
+﻿using System.Resources;
 
 using TomsToolbox.Core;
 
-[assembly: AssemblyTitle("SampleApp")]
-[assembly: AssemblyDescription("")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("tom-englert.de")]
-[assembly: AssemblyProduct("Tom's Toolbox")]
-[assembly: AssemblyCopyright("Copyright © 2018")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 [assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.MainAssembly)]
-[assembly: ComVisible(false)]
 
 [assembly: PluginModule("Core")]
 
