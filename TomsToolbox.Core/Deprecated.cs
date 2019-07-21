246fc2cd524d8e0e89dccaa19ebb1f705ec61e9c
tom-englert <mail@tom-englert.de>
2019-07-21 19:36:06 +0200

    external annotations are required to packages

--- TomsToolbox.Core/Deprecated.cs
+++ TomsToolbox.Core/Deprecated.cs
@@ -1,12 +1,23 @@
-﻿using System;
-
-namespace TomsToolbox.Core
+﻿namespace TomsToolbox.Core
 {
+    using System;
+
+    using JetBrains.Annotations;
+
     /// <summary>
     /// This module is deprecated. Code has been moved to TomsToolbox.Essentials to avoid naming confusions with .Net Core.
     /// </summary>
     [Obsolete("This module is deprecated. Code has been moved to TomsToolbox.Essentials", true)]
     public class Deprecated
     {
+        /// <summary>
+        /// Dummy method.
+        /// </summary>
+        /// <returns>null</returns>
+        [CanBeNull]
+        public object Dummy()
+        {
+            return null;
+        }
     }
 }
