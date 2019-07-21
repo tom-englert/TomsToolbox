f0dfe85921e910d9e7d6ea642bb70bb74e9045a8
tom-englert <mail@tom-englert.de>
2019-07-21 19:27:34 +0200

    Add dummy module for TomsToolbox.Core

---
+++ TomsToolbox.Core/Deprecated.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TomsToolbox.Core
+{
+    /// <summary>
+    /// This module is deprecated. Code has been moved to TomsToolbox.Essentials to avoid naming confusions with .Net Core.
+    /// </summary>
+    [Obsolete("This module is deprecated. Code has been moved to TomsToolbox.Essentials", true)]
+    public class Deprecated
+    {
+    }
+}
