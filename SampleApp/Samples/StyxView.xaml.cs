5c4ea5a5d92ca3f06edfc48e4d603f72f477e1fb
tom-englert <mail@tom-englert.de>
2019-09-29 15:33:16 +0200

    Fix grid splitter to work with 4px size

---
+++ SampleApp/Samples/StyxView.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace SampleApp.Samples
+{
+    using TomsToolbox.Wpf.Composition.AttributedModel;
+
+    /// <summary>
+    /// Interaction logic for Styx.xaml
+    /// </summary>
+    [DataTemplate(typeof(StyxViewModel))]
+    public partial class Styx
+    {
+        public Styx()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ SampleApp/Samples/StyxViewModel.cs
@@ -0,0 +1,16 @@
+﻿namespace SampleApp.Samples
+{
+    using PropertyChanged;
+
+    using TomsToolbox.Wpf.Composition.AttributedModel;
+
+    [VisualCompositionExport(RegionId.Main, Sequence=9)]
+    [ImplementPropertyChanged]
+    class StyxViewModel
+    {
+        public override string ToString()
+        {
+            return "Styx";
+        }
+    }
+}
