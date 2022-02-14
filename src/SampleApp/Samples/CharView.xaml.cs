672b84f02b2eedb62c74b2662a799df95367cce1
tom-englert <mail@tom-englert.de>
2022-02-14 14:02:49 +0100

    Add Chart sample

---
+++ src/SampleApp/Samples/CharView.xaml.cs
@@ -0,0 +1,16 @@
+﻿namespace SampleApp.Samples
+{
+    using TomsToolbox.Wpf.Composition.AttributedModel;
+
+    /// <summary>
+    /// Interaction logic for CharView.xaml
+    /// </summary>
+    [DataTemplate(typeof(ChartViewModel))]
+    public partial class CharView
+    {
+        public CharView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/TomsToolbox.Wpf/Converters/StarSizeConverter.cs
@@ -0,0 +1,23 @@
+﻿namespace TomsToolbox.Wpf.Converters
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    /// <summary>
+    /// Converts a double value into a star-size grid length.
+    /// </summary>
+    [ValueConversion(typeof(double), typeof(GridLength))]
+    public class StarSizeConverter : ValueConverter
+    {
+        /// <inheritdoc />
+        protected override object? Convert(object value, Type? targetType, object? parameter, CultureInfo? culture)
+        {
+            if (value is not double size)
+                return null;
+
+            return new GridLength(size, GridUnitType.Star);
+        }
+    }
+}
