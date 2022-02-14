9981e86c7ebe04f4c754a2e1949d758bbc4b89fa
tom-englert <mail@tom-englert.de>
2022-02-14 08:38:23 +0100

    Composition framework: Support Microsoft.Extensions.DependencyInjection

---
+++ src/TomsToolbox.Composition.MicrosoftExtensions/ExportProviderAdapter.cs
@@ -0,0 +1,91 @@
+﻿namespace TomsToolbox.Composition.MicrosoftExtensions
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using global::Microsoft.Extensions.DependencyInjection;
+    using global::Microsoft.Extensions.Options;
+
+    /// <summary>
+    /// An <see cref="IExportProvider"/> adapter for the Microsoft.Extensions.DependencyInjection <see cref="ServiceCollection"/>
+    /// </summary>
+    /// <seealso cref="IExportProvider" />
+    public class ExportProviderAdapter : IExportProvider
+    {
+        private readonly ServiceProvider _context;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ExportProviderAdapter"/> class.
+        /// </summary>
+        /// <param name="context">The context providing the exports.</param>
+        public ExportProviderAdapter(ServiceProvider context)
+        {
+            _context = context;
+        }
+
+#pragma warning disable CS0067
+        /// <inheritdoc />
+        public event EventHandler<EventArgs>? ExportsChanged;
+
+        T IExportProvider.GetExportedValue<T>(string? contractName) where T : class
+        {
+            if (string.IsNullOrEmpty(contractName))
+            {
+                return _context.GetRequiredService<T>();
+            }
+
+            return _context.GetRequiredService<IOptionsSnapshot<T>>().Get(contractName);
+        }
+
+        T? IExportProvider.GetExportedValueOrDefault<T>(string? contractName) where T : class
+        {
+            if (string.IsNullOrEmpty(contractName))
+            {
+                return _context.GetService<T>();
+            }
+
+            return _context.GetService<IOptionsSnapshot<T>>()?.Get(contractName);
+        }
+
+        bool IExportProvider.TryGetExportedValue<T>(string? contractName, [System.Diagnostics.CodeAnalysis.NotNullWhen(true)] out T? value) where T : class
+        {
+            return (value = ((IExportProvider)this).GetExportedValueOrDefault<T>()) != null;
+        }
+
+        IEnumerable<T> IExportProvider.GetExportedValues<T>(string? contractName) where T : class
+        {
+            if (string.IsNullOrEmpty(contractName))
+            {
+                return _context.GetServices<T>();
+            }
+
+            return _context
+                .GetServices<IOptionsSnapshot<T>>()
+                .Select(snapshot => snapshot.Get(contractName));
+        }
+
+        IEnumerable<IExport<object>> IExportProvider.GetExports(Type contractType, string? contractName)
+        {
+            var exportMethod = GetType().GetMethod(nameof(GetExports))?.MakeGenericMethod(contractType);
+            if (exportMethod == null)
+                throw new InvalidOperationException("Method not found: " + nameof(GetExports));
+            return (IEnumerable<IExport<object>>)exportMethod.Invoke(this, new object?[] { contractName });
+        }
+
+        /// <summary>
+        /// Gets the exports.
+        /// </summary>
+        /// <typeparam name="T">The type of the object</typeparam>
+        /// <param name="contractName">Name of the contract.</param>
+        /// <returns>The exported object.</returns>
+        public IEnumerable<IExport<object>> GetExports<T>(string? contractName)
+        {
+            return Enumerable.Empty<IExport<object>>();
+
+            //return _context
+            //    .GetExports<ExportFactory<T, IDictionary<string, object>>>(contractName)
+            //    .Select(item => new ExportAdapter<object>(() => item.CreateExport().Value, new MetadataAdapter(item.Metadata)));
+        }
+    }
+}
---
+++ src/TomsToolbox.Composition.Tests/MicrosoftExtensionsTest.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TomsToolbox.Composition.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.CodeAnalysis;
+    using System.Linq;
+    using System.Threading;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    using TomsToolbox.Essentials;
+
+    [TestClass]
+    public class MicrosoftExtensionsTest
+    {
+        [TestMethod]
+        public void RegistrationBehavior()
+        {
+            var serviceCollection = new ServiceCollection();
+
+            serviceCollection
+                .AddSingleton<SomeDependency>()
+                .AddSingleton<SomeSingleton>()
+                .AddTransient(provider => new Lazy<SomeSingleton, IMetadata>(provider.GetRequiredService<SomeSingleton>, new MetadataAdapter(new Dictionary<string, object> { ["Test"] = "Value1" })))
+                .AddTransient<IMetadata>(provider => provider.GetRequiredService<SomeSingleton>())
+                .AddTransient(provider => new Lazy<IMetadata, IMetadata>(provider.GetRequiredService<SomeSingleton>, new MetadataAdapter(new Dictionary<string, object> { ["Test"] = "Value1" })))
+                .AddTransient(provider => new Lazy<IMetadata, IMetadata>(provider.GetRequiredService<SomeSingleton>, new MetadataAdapter(new Dictionary<string, object> { ["Test"] = "Value2" })))
+                ;
+
+            var serviceCollection2 = new ServiceCollection();
+
+            serviceCollection2.AddRange(serviceCollection);
+
+            var serviceProvider = serviceCollection2.BuildServiceProvider();
+
+            var x1 = serviceProvider.GetService<IMetadata>();
+            var x2 = serviceProvider.GetService<IMetadata>();
+
+            var all = serviceProvider.GetServices<Lazy<IMetadata, IMetadata>>();
+            var specific = all.Single(item => item.Metadata.GetValue("Test") as string == "Value2");
+            var t4 = specific.Value;
+
+            var lazy1 = serviceProvider.GetRequiredService<Lazy<SomeSingleton, IMetadata>>();
+            var metadata1 = lazy1.Metadata;
+            var value1 = metadata1.GetValue("Test");
+
+            var lazy2 = serviceProvider.GetRequiredService<Lazy<IMetadata, IMetadata>>();
+            var lazy3 = serviceProvider.GetRequiredService<Lazy<IMetadata, IMetadata>>();
+            var metadata2 = lazy2.Metadata;
+            var value2 = metadata2.GetValue("Test");
+
+            var t1 = lazy1.Value;
+            var t2 = lazy2.Value;
+            var t3 = serviceProvider.GetRequiredService<IMetadata>();
+
+            Assert.AreEqual("Test1", t1.GetValue("Test"));
+            Assert.AreEqual("Test1", t2.GetValue("Test"));
+            Assert.AreEqual("Test1", t3.GetValue("Test"));
+            Assert.AreEqual("Test1", t4.GetValue("Test"));
+        }
+
+        private class SomeDependency : INotifyChanged
+        {
+            public event EventHandler? Changed;
+        }
+
+        private class SomeSingleton : IMetadata
+        {
+            private static int _instanceCounter;
+
+            private readonly SomeDependency _dependency;
+            private readonly int _instanceId;
+
+            public SomeSingleton(SomeDependency dependency)
+            {
+                _dependency = dependency;
+                _instanceId = Interlocked.Increment(ref _instanceCounter);
+            }
+
+            public object GetValue(string name)
+            {
+                return name + _instanceId;
+            }
+
+            public bool TryGetValue(string name, [NotNullWhen(true)] out object? value)
+            {
+                value = name + _instanceId;
+                return true;
+            }
+        }
+    }
+}
