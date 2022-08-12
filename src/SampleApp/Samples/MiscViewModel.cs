c986c0ae32ddc4f8a9cc1b6d60d4c7ae91ed6812
tom-englert <mail@tom-englert.de>
2022-08-12 14:32:50 +0200

    Add UAC sample for testing

--- src/SampleApp/Samples/MiscViewModel.cs
+++ src/SampleApp/Samples/MiscViewModel.cs
@@ -1,11 +1,13 @@
 ﻿namespace SampleApp.Samples
 {
     using System;
+    using System.Net;
     using System.Windows;
     using System.Windows.Input;
 
     using PropertyChanged;
 
+    using TomsToolbox.Desktop;
     using TomsToolbox.Wpf;
     using TomsToolbox.Wpf.Composition.AttributedModel;
 
@@ -13,6 +15,8 @@
     [AddINotifyPropertyChangedInterface]
     internal class MiscViewModel
     {
+        private string? _userName;
+
         public override string ToString()
         {
             return "Misc.";
@@ -25,5 +29,44 @@
         public ICommand ItemsControlDefaultCommand => new DelegateCommand<string>(item => MessageBox.Show(item + " clicked."));
 
         public ICommand GCCollectCommand => new DelegateCommand(GC.Collect);
+
+        public ICommand CheckForAdminRightsCommand => new DelegateCommand(CheckForAdminRights);
+
+        private void CheckForAdminRights()
+        {
+            bool CheckRights()
+            {
+                var credential = new NetworkCredential(_userName ?? string.Empty, string.Empty);
+                var lastError = 0;
+
+                var parent = Application.Current?.MainWindow?.GetWindowHandle() ?? IntPtr.Zero;
+
+                while ((credential = UserAccountControl.PromptForCredential(parent, "Test", "Login", lastError, credential)) != null)
+                {
+                    lastError = credential.LogOnInteractiveUser(out var userToken);
+
+                    using (userToken)
+                    {
+                        if (lastError != 0 || userToken == null)
+                        {
+                            continue;
+                        }
+
+                        if (UserAccountControl.IsUserInAdminGroup(userToken) ||
+                            UserAccountControl.IsUserInGroup(userToken, "LocalAdmins"))
+                        {
+                            _userName = credential.UserName;
+                            return true;
+                        }
+
+                        lastError = 740; // (0x2E4) ERROR_ELEVATION_REQUIRED, The requested operation requires elevation.
+                    }
+                }
+
+                return false;
+            }
+
+            MessageBox.Show(CheckRights() ? "User is Admin" : "User is not an Admin");
+        }
     }
 }
